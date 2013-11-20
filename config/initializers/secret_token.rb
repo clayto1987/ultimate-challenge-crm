5bac2c360bb63996f75a70004c9f4aaf0862c1ca
Clayton Wallace <cwallace@academic.rrc.ca>
2013-11-20 11:31:50 -0600

    First commit created crm challenge project and scaffolded customer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrmChallenge::Application.config.secret_token = 'edd774c6dd75c04002c85c205724d2f7eb9d168277e41aa0bdfe235187337e6d04fdbe50fbfba5778dba67fdc1d795e28340295116af6de5488f27cbdbf6b83e'
